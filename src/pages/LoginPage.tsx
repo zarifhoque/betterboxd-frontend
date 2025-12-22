0dc766d87a53fb9636c85cbbf0473c753a956162
zarifhoque <hoquelabid@gmail.com>
2025-12-22 10:50:19 +0600

    feat: add login form and prototype for themeContext

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import LoginForm from "../components/LoginForm";
+import { LoginForm } from "@/components/LoginForm";
+
+const handleLogin = async (data: any) => {
+    console.log("Login data:", data);
+};
 
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-background to-secondary/50 p-4">
-      <LoginForm />
+    <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-background to-secondary/50 p-4 w-full">
+      <LoginForm onSubmit={handleLogin} />
     </div>
   );
 }
